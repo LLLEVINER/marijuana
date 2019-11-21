cec653240c60687e1e5bbe218ac3eecc98c0e733
LLLEVINER <47662187+LLLEVINER@users.noreply.github.com>
2019-11-21 14:16:45 -0500

    added files

---
+++ map.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibGxsZXZpbmVyIiwiYSI6ImNqMGd1b3FvZjAwYXYzM3VqYWU1dWE4dzkifQ.VoYntMR5DrqojWJAPW3nQA';
+var map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/llleviner/ck24p7r1i5l6a1ck0xsgybkwk',
+
+});
